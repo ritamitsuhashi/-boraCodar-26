dcbcf10606252ea12d8a0f828624dd2ac9a1dbdf
Rita <ritamitsuhashi@gmail.com>
2023-07-08 16:56:02 -0300

    added decrease button js

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ function increaseCounter() {
     value++;
     counter.textContent = value.toString().padStart(2, "0");
 }
+
+function decreaseCounter() {
+    const counter = document.getElementById
+    ("counter");
+    let value = parseInt(counter.textContent);
+    if(value > 1) {
+        value--;
+        counter.textContent = value.toString().padStart(2, "0");
+    }
+}
