1fb6fda104d0bb4235e26ef63133ca0e36b7035c
Rita <ritamitsuhashi@gmail.com>
2023-07-08 17:43:08 -0300

    increase ingredients as portion value increases

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 function increaseCounter() {
-    const counter = document.getElementById
-    ("counter");
-    let value = parseInt(counter.textContent);
-    value++;
-    counter.textContent = value.toString().padStart(2, "0");
+  const counter = document.getElementById("counter");
+  let value = parseInt(counter.textContent);
+  value++;
+  counter.textContent = value.toString().padStart(2, "0");
+
+  const portions = document.getElementsByClassName("portion");
+  for (portion of portions) {
+    let porcaoInicial = parseInt(portion.textContent) / (value - 1);
+    portion.textContent = porcaoInicial * value;
+  }
 }
 
 function decreaseCounter() {
-    const counter = document.getElementById
-    ("counter");
-    let value = parseInt(counter.textContent);
-    if(value > 1) {
-        value--;
-        counter.textContent = value.toString().padStart(2, "0");
+  const counter = document.getElementById("counter");
+  let value = parseInt(counter.textContent);
+  if (value > 1) {
+    value--;
+    counter.textContent = value.toString().padStart(2, "0");
+
+    const portions = document.getElementsByClassName("portion");
+    for (portion of portions) {
+      let porcaoInicial = parseInt(portion.textContent) / (value + 1);
+      portion.textContent = porcaoInicial * value;
     }
+  }
 }
